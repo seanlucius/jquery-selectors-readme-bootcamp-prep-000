b88d50f6a8e26e91d4d3845aaf1bc3c9bd18a853
Sean Lucius <seanlucius22@icloud.com>
2018-09-01 02:20:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,6 +7,18 @@ function paragraphSelector() {
 
 function ninjaBabySelector() {
   
+  $('#baby-ninja');
+  
+}
+
+function divSelector() {
+  
+  $('.pics');
+  
+}
+
+function firstListItem() {
+  
   
   
 }
